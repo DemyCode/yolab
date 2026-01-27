dfec9f8195da34456f6aa9fd3cb606b0533a4b77
DemyCode <verycols@gmail.com>
2026-01-27 18:59:24 +0100

    feat: lets go

--- homelab/installer/iso-config.nix
+++ homelab/installer/iso-config.nix
@@ -40,13 +40,15 @@ in {
   networking.networkmanager.enable = true;
   networking.wireless.enable = lib.mkForce false;
 
-  environment.systemPackages = with pkgs; [
-    vim
-    curl
-    parted
-    gptfdisk
-    util-linux
-  ];
+  environment.systemPackages =
+    (with pkgs; [
+      vim
+      curl
+      parted
+      gptfdisk
+      util-linux
+    ])
+    ++ [installerBackend];
 
   # Auto-login to TTY1
   services.getty.autologinUser = "nixos";
