484f4dd748a55b7acd29487e7d649137aadd08de
DemyCode <verycols@gmail.com>
2026-03-31 11:57:27 +0200

    feat: lets go

--- homelab/nixos/common.nix
+++ homelab/nixos/common.nix
@@ -89,7 +89,7 @@ in
     services.k3s = {
       enable = true;
       role = "server";
-      token = k3sCfg.token or "caca";
+      token = k3sCfg.token or "";
       clusterInit = isFirstNode;
       serverAddr = lib.optionalString (!isFirstNode) (k3sCfg.server_addr or "");
       extraFlags = toString [
