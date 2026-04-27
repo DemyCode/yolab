5e625423af2dcc28104d0b7a5c83bd253e0f185f
DemyCode <verycols@gmail.com>
2026-04-27 22:20:04 +0200

    feat: adding swapspace

--- homelab/nixos/configuration.nix
+++ homelab/nixos/configuration.nix
@@ -4,12 +4,15 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     ./common.nix
-  ] ++ lib.optional (builtins.pathExists ../ignored/hardware-configuration.nix) ../ignored/hardware-configuration.nix;
+  ]
+  ++ lib.optional (builtins.pathExists ../ignored/hardware-configuration.nix) ../ignored/hardware-configuration.nix;
 
   boot.loader.systemd-boot.enable = true;
 
   networking.networkmanager.enable = true;
   users.users.homelab.extraGroups = [ "networkmanager" ];
+
+  services.swapspace.enable = true;
 
   system.stateVersion = "24.05";
 }
